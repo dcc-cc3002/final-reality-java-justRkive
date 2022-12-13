32e066a9fcefda727274137c2bbe5bf8b0c2b39f
Katia <katia.f.a.20@gmail.com>
2022-12-12 23:56:31 -0300

    listo

--- src/main/java/cl/uchile/dcc/finalreality/controller/GameController.java
+++ src/main/java/cl/uchile/dcc/finalreality/controller/GameController.java
@@ -1,4 +1,22 @@
 package cl.uchile.dcc.finalreality.controller;
 
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+
+/**
+ * The game controller.
+ *
+ * Class that makes the logic conexion between the view and the model. It manages
+ * the game and makes all the operations needed.
+ */
 public class GameController {
+  private GameCharacter player1;
+  private GameCharacter player2;
+  
+  
+  
+  
 }
+  
+  
+
+
